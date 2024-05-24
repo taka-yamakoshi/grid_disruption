63e5d06bd6c82c59e67768179d8b44a3780ecd98
Takateru Yamakoshi <takateru@kurome.riken.jp>
2024-05-24 18:15:58 +0900

    update classifier

--- classifier.py
+++ classifier.py
@@ -26,54 +26,40 @@ class MyDataset(torch.utils.data.Dataset):
         lbl = torch.tensor(self.labls[idx].astype(np.int64))
         return img, lbl
 
-def get_data_loader(
+def add_channels(data: np.ndarray,
+                 csize: int = 1):
+    assert data.shape[0]%csize==0
+    return data.reshape(data.shape[0]//csize,csize,*data.shape[1:])
+
+def get_loader(data_wt: np.ndarray,
+               data_tg: np.ndarray,
+               bsize: int = 100,
+               csize: int = 1):
+    data_wt = add_channels(data_wt, csize)
+    data_tg = add_channels(data_tg, csize)
+    data = np.vstack([data_wt,data_tg])
+    labs = np.array([0]*len(data_wt) + [1]*len(data_tg))
+    dataset = MyDataset(data, labs, transform=torchvision.transforms.Normalize(0.5,0.5,0.5))
+    return torch.utils.data.DataLoader(dataset, batch_size=bsize, shuffle=True, num_workers=4)
+
+def get_data(
         rmaps:dict,
-        split:Tuple[int,int,int] = [800,50,50],
-        batch_sizes: Tuple[int,int,int] = [64,100,100],
-        csize: int = 1,
+        split:Tuple[int,int,int] = [700,100,100],
         seed: int = 1234):
     # randomly select data
     rng = np.random.default_rng(seed)
     wtdata = rmaps['wta'][rng.permutation(len(rmaps['wta']))[:np.sum(split)]]
     tgdata = rmaps['j20a'][rng.permutation(len(rmaps['j20a']))[:np.sum(split)]]
 
-    # introduce channel dimension
-    assert wtdata.shape[0]%csize==0 & tgdata.shape[0]%csize==0, 'csize has to divide number of samples'
-    wtdata = wtdata.reshape(wtdata.shape[0]//csize,csize,*wtdata.shape[1:])
-    tgdata = tgdata.reshape(tgdata.shape[0]//csize,csize,*tgdata.shape[1:])
-
-    # train data
-    assert split[0]%csize==0, 'train split is not multiple of csize'
-    trn_split = split[0]//csize
-    trn_data = np.vstack([wtdata[:trn_split],tgdata[:trn_split]])
-    trn_labs = np.array([0]*trn_split + [1]*trn_split)
-
-    # val data
-    assert split[1]%csize==0, 'val split is not multiple of csize'
-    val_split = split[1]//csize
-    val_data = np.vstack([wtdata[trn_split:(trn_split+val_split)],tgdata[trn_split:(trn_split+val_split)]])
-    val_labs = np.array([0]*val_split + [1]*val_split)
-
-    # test data
-    assert split[2]%csize==0, 'test split is not multiple of csize'
-    tst_split = split[2]//csize
-    tst_data = np.vstack([wtdata[(trn_split+val_split):],tgdata[(trn_split+val_split):]])
-    tst_labs = np.array([0]*tst_split + [1]*tst_split)
-
-    assert trn_data.shape[0]==trn_labs.shape[0]
-    assert val_data.shape[0]==val_labs.shape[0]
-    assert tst_data.shape[0]==tst_labs.shape[0]
-    print(trn_data.shape, trn_labs.shape, val_data.shape, val_labs.shape, tst_data.shape, tst_labs.shape)
-
-    trn_dataset = MyDataset(trn_data, trn_labs, transform=torchvision.transforms.Normalize(0.5,0.5))
-    val_dataset = MyDataset(val_data, val_labs, transform=torchvision.transforms.Normalize(0.5,0.5))
-    tst_dataset = MyDataset(tst_data, tst_labs, transform=torchvision.transforms.Normalize(0.5,0.5))
-
-    trn_loader = torch.utils.data.DataLoader(trn_dataset, batch_size=batch_sizes[0], shuffle=True, num_workers=4)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_sizes[1], shuffle=True, num_workers=4)
-    tst_loader = torch.utils.data.DataLoader(tst_dataset, batch_size=batch_sizes[2], shuffle=True, num_workers=4)
-
-    return trn_loader, val_loader, tst_loader
+    trn_split = split[0]
+    val_split = split[1]
+    tst_split = split[2]
+    
+    trn_data = (wtdata[:trn_split], tgdata[:trn_split])
+    val_data = (wtdata[trn_split:(trn_split+val_split)],tgdata[trn_split:(trn_split+val_split)])
+    tst_data = (wtdata[(trn_split+val_split):],tgdata[(trn_split+val_split):])
+    assert tst_data[0].shape[0]==tst_split and tst_data[1].shape[0]==tst_split
+    return trn_data, val_data, tst_data
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -110,12 +96,15 @@ if __name__ == '__main__':
     print(f'Loaded {run_ID} --- Last modified at {time.ctime(os.stat(fname).st_mtime)}')
     rmaps = np.load(fname)
 
-    trn_loader, val_loader, tst_loader = get_data_loader(rmaps, split=[800,50,50], batch_sizes=[args.trn_bsize, args.val_bsize, args.tst_bsize], csize=args.csize, seed=args.seed)
+    trn_data, val_data, tst_data = get_data(rmaps, split=[700,100,100], seed=args.seed)
+    trn_loader = get_loader(trn_data[0], trn_data[1], args.trn_bsize, args.csize)
+    val_loader = get_loader(val_data[0], val_data[1], args.val_bsize, args.csize)
+    tst_loader = get_loader(tst_data[0], tst_data[1], args.tst_bsize, args.csize)
 
     config = ConvNextV2Config(
-        num_channels=args.csize, patch_size=args.patch_size, num_stages=4,
-        hidden_sizes=[args.hsize,2*args.hsize,4*args.hsize,8*args.hsize],
-        depths=[2,2,6,2], hidden_act='gelu', image_size=args.res, num_labels=2)
+        num_channels=args.csize, patch_size=args.patch_size, num_stages=3,
+        hidden_sizes=[args.hsize,2*args.hsize,4*args.hsize],
+        depths=[2,2,2], hidden_act='gelu', image_size=args.res, num_labels=2)
     model = ConvNextV2ForImageClassification(config)
 
     model.to(args.device)
@@ -125,14 +114,16 @@ if __name__ == '__main__':
     trn_loss = []
     val_loss = []
     val_acc_list = []
+    val_loss_best = np.inf
     for epoch in range(args.epochs):
+        rng = np.random.default_rng(args.seed+epoch)
+        trn_loader = get_loader(trn_data[0][rng.permutation(len(trn_data[0]))],
+                                trn_data[1][rng.permutation(len(trn_data[1]))],
+                                args.trn_bsize, args.csize)
         model.train()
         trn_loss_epoch = 0.0
         for data in trn_loader:
             inputs, labels = data
-            #inputs = inputs.reshape(inputs.shape[0]//args.csize,args.csize,*inputs.shape[1:])
-            print(inputs.shape)
-            exit()
             optimizer.zero_grad()
             outputs = model(inputs.to(args.device), labels.to(args.device))
             loss = outputs.loss
@@ -142,29 +133,35 @@ if __name__ == '__main__':
             trn_loss_epoch += loss.item()
         trn_loss.append(trn_loss_epoch)
 
-        if epoch%5==0:
-            model.eval()
-            val_loss_epoch = 0.0
-            val_acc = []
-            for data in val_loader:
-                inputs, labels = data
-                inputs = inputs.unsqueeze(1)
-                with torch.no_grad():
-                    outputs = model(inputs.to(args.device),labels.to(args.device))
-                pred = (F.softmax(outputs.logits,dim=-1)[:,1] > 0.5).to('cpu').to(float)
-                val_acc.extend(list((labels.to(float)==pred).numpy()))
 
-                loss = outputs.loss
-                val_loss_epoch += loss.item()
-            val_loss.append(val_loss_epoch)
-            val_acc_list.append(np.mean(val_acc))
-            print(f'Epoch {epoch} / Train Loss: {trn_loss_epoch}, Val Loss: {val_loss_epoch}, Val Acc: {np.mean(val_acc)}')
+        val_loader = get_loader(val_data[0][rng.permutation(len(val_data[0]))],
+                                val_data[1][rng.permutation(len(val_data[1]))],
+                                args.val_bsize, args.csize)
+        model.eval()
+        val_loss_epoch = 0.0
+        val_acc = []
+        for data in val_loader:
+            inputs, labels = data
+            with torch.no_grad():
+                outputs = model(inputs.to(args.device),labels.to(args.device))
+            pred = (F.softmax(outputs.logits,dim=-1)[:,1] > 0.5).to('cpu').to(float)
+            val_acc.extend(list((labels.to(float)==pred).numpy()))
 
+            loss = outputs.loss
+            val_loss_epoch += loss.item()
+        if val_loss_epoch < val_loss_best:
+            model.save_pretrained(f'data/classifier/{model_name}')
+            val_loss_best = val_loss_epoch
+        val_loss.append(val_loss_epoch)
+        val_acc_list.append(np.mean(val_acc))
+        print(f'Epoch {epoch} / Train Loss: {trn_loss_epoch}, Val Loss: {val_loss_epoch}, Val Acc: {np.mean(val_acc)}')
+
+    model = ConvNextV2ForImageClassification.from_pretrained(f'data/classifier/{model_name}')
+    model.to(args.device)
     model.eval()
     tst_acc = []
     for data in tst_loader:
         inputs, labels = data
-        inputs = inputs.unsqueeze(1)
         with torch.no_grad():
             outputs = model(inputs.to(args.device),labels.to(args.device))
         pred = (F.softmax(outputs.logits,dim=-1)[:,1] > 0.5).to('cpu').to(float)
@@ -173,5 +170,3 @@ if __name__ == '__main__':
 
     out_dict = {'trn_loss':np.array(trn_loss), 'val_loss':np.array(val_loss), 'val_acc':np.array(val_acc_list), 'tst_acc':np.mean(tst_acc)}
     np.savez(f'data/classifier/{model_name}/stats.npz', **out_dict)
-
-    model.save_pretrained(f'data/classifier/{model_name}')
